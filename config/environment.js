040061cbfc26fde1920f6ecdcd74d4339682725a
Benjamin Rosas <ben@aliencyb.org>
2020-04-29 15:19:45 -0500

    deployment needs

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,9 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+    fastboot: {
+      hostWhitelist: [/^(\w+\.)?impulsesieve.art$/, /^localhost:\d+$/]
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
