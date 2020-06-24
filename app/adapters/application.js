1cc35b696a8d0d98cc99972f71f07168ff082d8c
Benjamin Rosas <ben@aliencyb.org>
2020-06-23 23:29:33 -0500

    should be ok without this

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -10,9 +10,7 @@ export default class ApplicationAdapter extends RESTAdapter {
     const collectionName = pluralize(modelName)
 
     const response = await super.findAll(...arguments)
-    if (response.isAdapterError) {
-      throw response
-    }
+
     return { [collectionName]: response }
   }
 
@@ -21,9 +19,7 @@ export default class ApplicationAdapter extends RESTAdapter {
     const collectionName = pluralize(modelName)
 
     const response = await super.query(...arguments)
-    if (response.isAdapterError) {
-      throw response
-    }
+
     return { [collectionName]: response }
   }
 }
